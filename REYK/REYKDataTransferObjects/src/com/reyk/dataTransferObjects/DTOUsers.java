0403f317a03ba0f1a1240099b58d83b9e00e198c
MacAA <MacAA@172.20.2.231>
2015-11-03 20:45:06 -0200

    DTOs

---
+++ REYK/REYKDataTransferObjects/src/com/reyk/dataTransferObjects/DTOUsers.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.reyk.dataTransferObjects;
+
+/**
+ *
+ * @author MacAA
+ */
+public class DTOUsers {
+    
+    private Long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+    private String email;
+
+    public DTOUsers(String name, String surname, String username, String password, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public DTOUsers() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+   }
